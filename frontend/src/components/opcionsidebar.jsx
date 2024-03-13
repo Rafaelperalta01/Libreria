4c49821c05daf9e6571702b0e7ed1aaf5965cafb
Rafael Peralta <124094001+Rafaelperalta01@users.noreply.github.com>
2024-03-13 20:21:50 -0300

    Sidebar y dashboard responsive

--- frontend/src/components/opcionsidebar.jsx
+++ frontend/src/components/opcionsidebar.jsx
@@ -4,7 +4,7 @@ export default function Option({ imagen, texto }){
     return(
         <div className=' rounded p-2 hover:bg-blue-800 transition duration-300 hover:cursor-pointer'>
             <img className='h-10 invert mx-auto' src={imagen} alt="Libros" />
-            <li className="pt-2">{texto}</li>
+            <li className="hidden pt-2 lg:block">{texto}</li>
         </div>
     )
 }
